ee6754d98cd0d7bf25a6e4a8c8b7edf43f99218b
Theo M <theodoremoreland@yahoo.com>
2019-06-02 03:33:44 -0500

    added clientScrape.js and base.html

---
+++ static/clientScrape.js
@@ -0,0 +1,60 @@
+function scrape() {
+
+    var profile;
+    var response;
+
+    !function doStuff() {
+
+        profile = prompt("Please enter your twitter handle", "lakers").toLowerCase();
+        setTimeout(continueExecution, 4000) // wait four seconds before continuing
+        $.getJSON('http://www.whateverorigin.org/get?url=' + 
+            encodeURIComponent('https://twitter.com/' + profile) + '&callback=?', function(data) {
+            // console.log(data.contents);
+            response = data.contents; })
+        }();
+
+    function continueExecution() {
+
+        var tag_start_index = response.search(/img class="ProfileAvatar-image/g);
+
+        var tagEstimate =  response.slice(tag_start_index, tag_start_index + 200);
+        
+        if (tagEstimate.indexOf(".jpg") !== -1) {
+            var pic_url_end = tagEstimate.indexOf(".jpg") + 4;
+        }
+
+        else if (tagEstimate.indexOf(".jpeg") !== -1) {
+            var pic_url_end = tagEstimate.indexOf(".jpeg") + 5;
+        }
+
+        else if (tagEstimate.indexOf(".png") !== -1) {
+            var pic_url_end = tagEstimate.indexOf(".png") + 4;
+        }
+
+        else if (tagEstimate.indexOf(".gif") !== -1) {
+            var pic_url_end = tagEstimate.indexOf(".gif") + 4;
+        }
+
+        var imgTag = tagEstimate.slice(0, pic_url_end);
+        
+        var pic_url_start = imgTag.lastIndexOf('"') + 1;
+
+        var img = imgTag.slice(pic_url_start);
+
+        // console.log(img);
+
+            if (profile != null) {
+                document.getElementById("tweets").innerHTML = "";
+                document.getElementById("pic").innerHTML = "<img src=" + img + "> <br> <a href='https://twitter.com/" + profile + "' target='_blank'>Twitter</a>";
+            }
+
+           var tweets = $(response).find('p.TweetTextSize.TweetTextSize--normal.js-tweet-text.tweet-text').get();
+
+           tweets.forEach((t, index) => {
+                var li = document.createElement("li");                       // Create a <li> node
+                var tweet = document.createTextNode(tweets[index].innerText.replace("http", ' http').replace("pic", " pic"));   // Create a text node
+                li.appendChild(tweet);                                       // Append the text to <li>
+                document.getElementById("tweets").appendChild(li);           // Append <li> to <ul> with id="tweets"
+           })
+    }
+  }
